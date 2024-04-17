b59756173dba028b72c307d31f607a6f1429f9b2
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-04-18 01:59:11 +0900

    fix(shell): bash error

--- pckcfg.sh
+++ pckcfg.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # Should be run by go generate. DO NOT run it directly.
 
-files=($(find $@ | grep -v .DS_Store | sort))
+files=()
+while IFS= read -r line; do
+    files+=("$line")
+done <<< "$(find "$@" | grep -v .DS_Store | sort)"
 
 rm -rf cfg.zip
 zip -9 cfg.zip "${files[@]}"
